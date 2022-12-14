2f3b19298b2943342a970647037c884f2327608c
SamuelOshin <samuelt.oshin@gmail.com>
2022-12-14 09:18:57 -0800

    print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,8 +6,8 @@
 * Return: Always 0 (Success)
 */
 
-    int main(void)
-    {
+int main(void)
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -18,4 +18,4 @@
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-    }
+}
